1dae3ca85c1fb807b200ad2f21af29cf50e10481
Viktor Freiman <freiman.viktor@gmail.com>
2024-01-01 17:52:47 +0000

    Update version and add timeout to request

--- py_dev_init/__init__.py
+++ py_dev_init/__init__.py
@@ -1,18 +1,17 @@
-"""
-This is a docstring
+"""This is a docstring.
 
 `Docs how to write docstring
 <https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html>`_
 """
 import requests
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 
 def main():
-    """Make a requests to "https://example.com/" """
+    """Make a requests to "https://example.com/" and print the status code."""
     url = "https://example.com/"
-    r = requests.head(url)
+    r = requests.head(url, timeout=5)
 
     print(f"{url} returned {r.status_code}")
 
